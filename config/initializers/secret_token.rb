457766dacf9aa0ab51e58df75aacdb3f883b46b0
Max Norton <maxbnorton@gmail.com>
2014-09-05 12:39:36 -0700

    prelaunch cleanup

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Prelaunchr::Application.config.secret_token = ENV["RAILS_SECRET"] || 'xxxxxxxx-change-me-please-xxxxxxxx'
+Prelaunchr::Application.config.secret_token = ENV["RAILS_SECRET"] || 'd032ee0508b72d5e32f9a3efab327aac789cac38c3715063ee10d7cc9299dd91b7ec65a09488a42599eb97222747a3dc150c1c1a2afe9a398a757ccac35c637c'
