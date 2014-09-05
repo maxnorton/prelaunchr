dc982ef1857a935fd07e356f9438827fc7b3a60b
Max Norton <maxbnorton@gmail.com>
2014-09-04 22:05:38 -0700

    second test update of assets paths

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Prelaunchr::Application.configure do
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'www.example.com' }
+  config.action_mailer.default_url_options = { :host => 'powerful-lake-3411.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   #  config.action_mailer.smtp_settings = { :address=> 'localhost', :port=>1025 }
