79fcbd838b43e7c3f9cc67e6be84c8f44481ec6d
Max Norton <maxbnorton@gmail.com>
2014-09-05 15:07:53 -0700

    make passwords in db/seeds.rb match

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(:email => 'tor@ericksonwoodworking.com', :password => 'businesslambshark', :password_confirmation => 'changeme')
+AdminUser.create!(:email => 'tor@ericksonwoodworking.com', :password => 'businesslambshark', :password_confirmation => 'businesslambshark')
