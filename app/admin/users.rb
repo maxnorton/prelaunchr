d537c090e674d86cf221981b75d8c2526c31a4f1
Max Norton <maxbnorton@gmail.com>
2014-09-08 19:40:44 -0700

    correct content in dashboard referrer/referral filters, and allow sorting by referrer_id

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -2,14 +2,14 @@ ActiveAdmin.register User do
   actions :index, :show
 
   preserve_default_filters!
-  #filter :referrer, collection: User.all.map{ |user| [user.email, user.id] }
-  #filter :referrals, collection: User.all.map{ |user| [user.email, user.id] }
+  filter :referrer, collection: User.all.map{ |user| [user.email, user.id] }
+  filter :referrals, collection: User.all.map{ |user| [user.email, user.id] }
 
   index do 
   	column :id
   	column("Name") 				{ |user| user.first_name + " " + user.last_name }
  	column :email
-  	column "Referrer", :referrer
+  	column("Referrer", :referrer, :sortable => :referrer_id)
   	column :referral_code
   	column("Created at", :created_at, :sortable => :created_at)
   	column("Referral count") 	{ |user| user.referrals.count }
